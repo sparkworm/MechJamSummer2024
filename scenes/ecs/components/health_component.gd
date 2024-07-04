92caca79790131c6f80e76a192a5814255c867e6
Sparkworm <d.clutter2337@gmail.com>
2024-07-04 12:42:01 -0500

    Added a basic health component

---
+++ scenes/ecs/components/health_component.gd
@@ -0,0 +1,25 @@
+class_name HealthComponent
+extends Node
+
+## emitted when health is 0 or below
+signal killed
+
+## the maximum amount of health possible, also the starting health
+@export var max_health: int
+## the actual amount of health
+var health: int
+
+func on_ready() -> void:
+	# set the actual health to the max_health
+	health = max_health
+
+## subtracts a specified number from health, and if health reaches 0, killed is 
+## emitted
+func damage(amnt: int) -> void:
+	health -= amnt
+	if health <= 0:
+		killed.emit()
+
+## adds a specified amount of health, but not exceeding max_health
+func heal(amnt: int) -> void:
+	health = min(max_health, health+amnt)
