8a66030d805e991d31803c93ad455b21a6906361
maochite <mao@jpkerrigan.dev>
2024-07-09 03:53:08 -0400

    Combined LevelManager with GameManager

--- scripts/autoload/GameManager.gd
+++ scripts/autoload/GameManager.gd
@@ -12,18 +12,25 @@ enum GameState {
 @export var _load_scene: PackedScene
 @export var _intro_scene: PackedScene
 @export var _game_scene: PackedScene
+@export var _levels: Array[PackedScene] = []
 
 var _game_state: GameState = GameState.Load
 var _current_scene: Node
+
+var _current_level_index: int = -1
+var _current_level_scene: Level = null
+var current_level_scene: Level:
+	get:
+		return _current_level_scene
 
 func _ready() -> void:
 	#should start in loading scene via project settings
 	pass
 
 func change_state(state: GameState) -> void:
-	call_deferred("_change_state", state)
+	call_deferred("_change_state_deffered", state)
 
-func _change_state(state: GameState) -> void:
+func _change_state_deffered(state: GameState) -> void:
 	_state_clean_up()
 	_game_state = state
 	_state_initialization()
@@ -34,6 +41,8 @@ func _state_clean_up() -> void:
 	elif(_game_state == GameState.Intro):
 		pass
 	else: #(_game_state == GameState.Game):
+		_current_level_scene.free()
+		_current_level_index = -1
 		pass
 
 	_current_scene.free()
@@ -48,6 +57,30 @@ func _state_initialization() -> void:
 
 	get_tree().root.add_child(_current_scene)
 	get_tree().current_scene = _current_scene
+
+func change_level(level_index: int) -> void:
+	if(_game_state != GameState.Game):
+		print("GameManager: cannot change level while in incorrect GameState")
+		return
+
+	if(level_index + 1 > _levels.size()):
+		print("GameManager: cannot change level with incorrect Level index")
+		return
+
+	var next_level: PackedScene = _levels[level_index]
+	call_deferred("_on_change_level_deffered", next_level)
+	_current_level_index = level_index
+
+func change_to_next_level():
+	change_level(_current_level_index + 1)
+
+func _on_change_level_deffered(next_level: PackedScene) -> void:
+	if(_current_level_scene != null):
+		_current_level_scene.free()
+
+	_current_level_scene = next_level.instantiate()
+	_current_scene.add_child(_current_level_scene)
+	#Change player position and stuff here according to the Level data
 
 func get_current_delta_time() -> float:
 	if(Engine.is_in_physics_frame):
